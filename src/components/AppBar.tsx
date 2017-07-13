863db0a280472b6678b6e75581d77bf29da10261
Jack Lightfoot <jack.lightfoot@tee2.org>
2017-07-13 12:50:47 -0700

    logo and title in app bar look ok

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -15,8 +15,9 @@ export interface State {
 
 const getTitleIcon = (title) => {
   return <div>
-           <div style={{position: 'relative',top: 4}} ><img style={{width: 40}}src={appIcon} />
-             {title}
+           <div style={{position: 'relative',top: 4}} >
+             <img style={{width: 40, display: 'block', float: 'left'}} src={appIcon} />
+             <div style={{position: 'relative', top: -5, left: 5}}>{title}</div>
            </div>
          </div>;
 }
