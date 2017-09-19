f7172a915a0879bb259763ec4cb06c784f09a9f2
Jack Lightfoot <jack.lightfoot@tee2.org>
2017-09-19 13:09:13 -0700

    made minor ui adjustment external link images - fixed issue running jest tests that was caused by case sensitive file extension configuration

--- src/components/ListItemExternal.tsx
+++ src/components/ListItemExternal.tsx
@@ -11,7 +11,7 @@ interface Props{
 const ListItemExternal: React.SFC<Props> = ({text,absoluteUrl,imageSrc}) => {
   const imageTitle = <div style={{position: 'relative'}}>
                         <img style={{position: 'absolute', top: -12, width: 40, height: 40}} src={imageSrc} />
-                        <span style={{position: 'relative',left: 45}}>{text}</span>
+                        <span style={{position: 'relative',left: 50}}>{text}</span>
                      </div>;
   return <ExternalLink absolutePath={absoluteUrl} ><ListItem primaryText={imageTitle} rightIcon={<BrowserIcon />} /></ExternalLink>;
 };
